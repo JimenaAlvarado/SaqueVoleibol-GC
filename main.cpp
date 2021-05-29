109e3609815d72022a2b462450d72805b423d62f
JimenaAlvarado <jimena.alva.cas@hotmail.com>
2021-05-29 16:49:50 -0500

    Visualización de objetos en una ventana

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "include/objreader/Object.hpp"
 #include "include/graphics/Trajectory.hpp"
+#include "include/graphics/Transformation.hpp"
+#include "GLFW/glfw3.h"
 #include <vector>
 
 using namespace std;
@@ -8,17 +10,98 @@ int main()
 {
     Object ball("obj/ball.obj");
     Object hand("obj/hand.obj");
+    Object net("obj/net.obj");
 
+    Transformation tr;
     Trajectory tray;
-    Vertex p1(-1, -1, 0.0);
+    Vertex p1(-0.9, -0.5, 0.0);
     Vertex p2(-0.8, 0.5, 0.0);
-    Vertex p3(-0.2, 0.5, 0.0);
-    Vertex p4(0.0, -1, 0.0);
-    vector <Vertex> curve = tray.curve(p1, p2, p3, p4, 0.1);
+    Vertex p3(0.8, 0.5, 0.0);
+    Vertex p4(0.5, -0.5, 0.0);
+    vector <Vertex> curve = tray.curve(p1, p2, p3, p4, 0.01);
+    int index_curve = 0;
 
     cout << "Curva de Bezier" << endl;
     for(Vertex v : curve)
-        cout << v.X() << " " << v.Y() << " " << v.Z() << endl;
+        cout << v.X() << " " << v.Y() << " " << v.Z() << endl; 
+
+    //Aplicar transformaciones para el balón.
+    arma::fmat Mtr_ball = tr.T(-0.7, -0.6, 0.0);
+    //Aplicar transformaciones para la mano.
+    arma::fmat Mtr_hand = tr.T(-0.8, -0.5, 0.0) * tr.Ry(-90);
+    //Aplicar transformaciones para la red.
+    arma::fmat Mtr_net = tr.S(0.8, 0.8, 0.8) * tr.T(2, -1.2, 0);
+
+    //Dibujar en ventana
+    GLFWwindow* window;
+
+    if( !glfwInit() )
+    {
+        cout << stderr << "Fallo al inicializar GLFW" << endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(800, 600, "Saque de voleibol", NULL, NULL);
+    if( window == NULL ) {
+        cout << stderr << "Fallo al abrir la ventana de GLFW." << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    glClearColor(0.8f, 0.8f, 0.8f, 0.0f);
+
+    do {
+        glClear( GL_COLOR_BUFFER_BIT );
+
+        glBegin(GL_TRIANGLES);
+
+            //Dibujar balón
+            glColor3f(1.0, 1.0, 1.0);
+            for(Face f : ball.GetFaces())
+            {   
+                for(Vertex v : f.GetVertices())
+                {
+                    arma::fcolvec v_tr = Mtr_ball * v.Homog();
+                    v_tr = v_tr / v_tr[3];
+                    glVertex3f(v_tr[0], v_tr[1], v_tr[3]);
+                }
+            }
+
+            //Dibujar mano (izquierda)
+            glColor3f(1.0, 1.0, 0.0);
+            for(Face f : hand.GetFaces())
+            {   
+                for(Vertex v : f.GetVertices())
+                {
+                    arma::fcolvec v_tr = Mtr_hand * v.Homog();
+                    v_tr = v_tr / v_tr[3];
+                    glVertex3f(v_tr[0], v_tr[1], v_tr[3]);
+                }
+            }
+
+            //Dibujar red
+            glColor3f(0.0, 0.0, 0.0);
+            for(Face f : net.GetFaces())
+            {   
+                for(Vertex v : f.GetVertices())
+                {
+                    arma::fcolvec v_tr = Mtr_net * v.Homog();
+                    v_tr = v_tr / v_tr[3];
+                    glVertex3f(v_tr[0], v_tr[1], v_tr[3]);
+                }
+            }
+
+        glEnd();
+      
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    
+    } while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+           glfwWindowShouldClose(window) == 0 );
+
+    glfwTerminate();	
 
     return 0;
 }   
